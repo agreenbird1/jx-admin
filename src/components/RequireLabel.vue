18a74fa2ba5eb4ced7dd6d9c94d8d38714e0f059
afanti <afanti@juexiaotime.com>
2022-07-22 14:13:11 +0800

    feat: 添加标记公共组件

---
+++ src/components/RequireLabel.vue
@@ -0,0 +1,24 @@
+<template>
+  <span>{{ text }}</span>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  text: string;
+}>();
+</script>
+
+<style scoped lang="less">
+span {
+  position: relative;
+  display: inline-block;
+  margin-right: 20px;
+  &::after {
+    position: absolute;
+    top: 0;
+    right: -8px;
+    content: "*";
+    color: @dangerColor;
+  }
+}
+</style>
