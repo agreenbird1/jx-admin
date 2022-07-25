63ecb819dcea222e380677338d2bb488f05a2c95
afanti <afanti@juexiaotime.com>
2022-07-25 11:30:44 +0800

    fix: 添加switch公共组件

---
+++ src/components/MySwitch.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="my-switch">
+    <span class="label">{{ label }}</span>
+    <span class="switch-bar" @click="changeSwitch">
+      <span
+        class="switch-dot"
+        :class="modelValue ? 'active' : 'de-active'"
+      ></span>
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  label?: string;
+  modelValue: boolean;
+}>();
+const emit = defineEmits(["update:modelValue"]);
+
+const changeSwitch = (e: Event) => {
+  e.stopPropagation();
+  emit("update:modelValue", !props.modelValue);
+};
+</script>
+
+<style scoped lang="less">
+.my-switch {
+  text-align: center;
+  display: inline-block;
+  span {
+    display: inline-block;
+  }
+  .label {
+    color: #aaa;
+  }
+  .switch-bar {
+    position: relative;
+    transform: translateY(2px);
+    background-color: #aaa;
+    border-radius: 20px;
+    margin-left: 10px;
+    width: 30px;
+    height: 14px;
+    .active {
+      right: -5px;
+      background-color: @jxColor;
+    }
+    .de-active {
+      right: 100% - 50px;
+      background-color: #c7c7cc;
+    }
+    .switch-dot {
+      position: absolute;
+      top: -2px;
+      height: 18px;
+      width: 18px;
+      border-radius: 9px;
+      transition: all 0.3s ease;
+    }
+  }
+}
+</style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,12 +3,14 @@ import AsideMenu from "./AsideMenu.vue";
 import MainHeader from "./MainHeader.vue";
 import MyTable from "./MyTable.vue";
 import RequireLabel from "./RequireLabel.vue";
+import MySwitch from "./MySwitch.vue";
 
 AsideMenu.name = "aside-menu";
 MainHeader.name = "main-header";
 MyTable.name = "my-table";
 RequireLabel.name = "require-label";
-const components = [RequireLabel, MyTable, MainHeader, AsideMenu];
+MySwitch.name = "my-switch";
+const components = [RequireLabel, MyTable, MainHeader, AsideMenu, MySwitch];
 
 export default {
   install: (app: App) => {
